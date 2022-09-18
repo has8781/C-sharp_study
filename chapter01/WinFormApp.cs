6f6e16f70f814412ee4d2ffba8b7a410f4f68bfb
has8781 <has8781@gmail.com>
2022-09-18 22:19:48 +0900

    chapter01 실습

---
+++ chapter01/WinFormApp.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows.Forms;
+class WinFormApp : Form {
+WinFormApp() {
+Text = "Hello World!";
+}
+public static void Main() {
+Application.Run(new WinFormApp());
+}
+} 
---
+++ chapter01/WinForm_console/WinFormApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+class WinFormApp : Form
+{
+    WinFormApp()
+    {
+        Text = "Hello World!";
+    }
+    public static void Main()
+    {
+        Application.Run(new WinFormApp());
+    }
+}
