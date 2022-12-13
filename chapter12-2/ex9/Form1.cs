a948aa35f0fb4e48b8578f03a7d20468acf9f722
has8781 <has8781@gmail.com>
2022-12-13 20:21:52 +0900

    chapter12 실습 12-ex9

---
+++ chapter12-2/ex9/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Rectangle r = new Rectangle(50, 50, 150, 100);  // 기준점 (50, 50), 폭과 넓이(150, 100)
+            g.DrawArc(Pens.Red, r, 45, 270);    // 시작 각도 x축 기준 45, 호각 270도
+        }
+    }
+}
