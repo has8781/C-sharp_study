a888fd96e1a70580c68a8304e6abfd1eff1e5bdc
has8781 <has8781@gmail.com>
2022-12-13 20:26:55 +0900

    chapter12 실습 12-ex25

---
+++ chapter12-2/ex25/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Image img = new Bitmap("vs.png");
+            Point[] pts = {
+            new Point(100, 0), new Point(100, 200), new Point(0, 0)
+            };
+            g.DrawImage(img, pts);
+
+        }
+    }
+}
