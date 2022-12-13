f77800b04085d5cde3c9d5b228d3a73bd59bff02
has8781 <has8781@gmail.com>
2022-12-13 20:06:32 +0900

    chapter08 실습 8-ex

---
+++ chapter08-2/ex/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.ReadOnly = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // +
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Text = "result = " + (a + b);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // -
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Text = "result = " + (a - b);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // *
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Text = "result = " + (a * b);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // /
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Text = "result = " + (a / b);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 종료
+            Close(); 
+        }
+    }
+}
