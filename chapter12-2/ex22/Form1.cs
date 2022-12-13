b2be03cb16c164dac39e51f8a383673b70f99cbc
has8781 <has8781@gmail.com>
2022-12-13 20:26:08 +0900

    chapter12 실습 12-ex22

---
+++ chapter12-2/ex22/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Image img = new Bitmap("vs.png");
+            g.DrawImage(img, ClientRectangle); // 지정 영역에 그림
+                                               // 영역에 그릴 경우, 그려질 영역의 크기가 이미지의 크기와
+                                               // 다를 경우, 이미지는 자동으로 축소되거나 확대되어 그려짐
+                                               // (TextureBrush와 달리 반복되지 않고, 이미지가 확대)
+        }
+    }
+}
