67838c3dc895b833f1987a491cb33c587a1773e5
has8781 <has8781@gmail.com>
2022-12-13 20:17:02 +0900

    chapter12 실습 12-4

---
+++ chapter12-1/12-4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+class RectContainsApp {
+    public static void Main() {
+        Rectangle rect = new Rectangle(10, 20, 80, 40);
+        Point pt1 = new Point(30, 40);
+        Point pt2 = new Point(90, 60);
+        Console.WriteLine("포함되는가? : {0}", rect.Contains(pt1));
+        Console.WriteLine("포함되는가? : {0}", rect.Contains(pt2));
+    }
+}
