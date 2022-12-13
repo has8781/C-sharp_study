22cf880d22db59e448a42d8ec3945f1e64d9e671
has8781 <has8781@gmail.com>
2022-12-13 20:22:06 +0900

    chapter12 실습 12-ex10

---
+++ chapter12-2/ex10/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.FillPie(Brushes.LightGreen, r, 45, 270);
+            g.DrawPie(Pens.DarkGreen, r, 45, 270);
+        }
+    }
+}
