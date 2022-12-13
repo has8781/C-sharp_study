771f008adb66a1ee23b8c9b15cc564b5d9c05847
has8781 <has8781@gmail.com>
2022-12-13 20:09:25 +0900

    chapter10 실습 10-ex1

---
+++ chapter10/ex1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _32 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawBorderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graphics objGraphics = CreateGraphics();
+            objGraphics.DrawRectangle(Pens.Blue,
+            pictureBox1.Left - 2, pictureBox1.Top - 2,
+            pictureBox1.Width + 2, pictureBox1.Height + 2);
+            objGraphics.Dispose();
+        }
+    }
+}
