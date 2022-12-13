87ec8deed049c30e65b1d58c1d8f65819b7a0705
has8781 <has8781@gmail.com>
2022-12-13 20:25:39 +0900

    chapter12 실습 12-ex20

---
+++ chapter12-2/ex20/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            string s = "This string is long enough to wrap. "; // 출력할 문자열
+            s += "We'll use a 15pt font, and assume ";
+            s += "the text string must fit into a width of 250 pixels. ";
+            Font f = new Font("Tahoma", 15); // 서체는 타호마, 글자 크기는 15.
+            SizeF sf = g.MeasureString(s, f, 250); // 영역의 크기를 측정한다.
+            RectangleF rf = new RectangleF(20, 20, sf.Width, sf.Height);
+            Rectangle r = Rectangle.Ceiling(rf); // 올림으로 변환
+            g.DrawString(s, f, Brushes.Black, r); // 문자열을 출력한다.
+            g.DrawRectangle(Pens.Black, r); // 외곽선을 그린다.
+            f.Dispose();
+        }
+    }
+}
