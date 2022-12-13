e0b60c83a627ea74df937d62f458d9c99298f482
has8781 <has8781@gmail.com>
2022-12-13 20:27:22 +0900

    chapter12 실습 12-ex27

---
+++ chapter12-2/ex27/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            SolidBrush b = new SolidBrush(Color.Lime);
+            g.FillRectangle(b, ClientRectangle);
+            b.Dispose();
+        }
+    }
+}
